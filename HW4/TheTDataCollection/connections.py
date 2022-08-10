117fbd4a0cbd5d4e74f3653b8046ca4f08cae2d8
pattonchow <40862752+pattonchow@users.noreply.github.com>
2022-08-10 16:42:26 -0400

    Add files via upload

---
+++ HW4/TheTDataCollection/connections.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def main():
+    #file_path = "C:\Users\patto\Desktop\课程\dv5642\HW\HW4\HW4-1\HW4-code\data.html"
+    file_name = "data.html"
+    table = pd.read_html(file_name)
+    print("table count:", len(table))
+
+    data_frame1 = table[0]
+    data_frame2 = table[1]
+
+    #print(data_frame1)
+    data_frame2.to_csv("connections.csv", index = False)
+
+if __name__ == "__main__":
+    main()
